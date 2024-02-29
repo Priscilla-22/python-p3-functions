46f54501dca1124b5253799abb63650ed170a58a
Priscilla-22 <priscillamw123@gmail.com>
2024-02-29 21:56:38 +0300

    feat: functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet("Priscilla")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default("Mary")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+add(2, 2)
+
 
 def halve(number):
-    pass
+    return number / 2
